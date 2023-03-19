33fd1bbf0ee4f53ea80703a33a35370b5e45c837
Danyil <126832639+Leyofficial@users.noreply.github.com>
2023-03-19 13:40:22 +0000

    First deploy

--- js/main.js
+++ js/main.js
@@ -15,6 +15,14 @@ function open(evt) {
   tabTarget.classList.add("tabs__btn-item--active");
 }
 
+const menuBtn =document.querySelector(".menu__btn")
+const menu =document.querySelector(".menu__list")
+
+menuBtn.addEventListener("click" , () =>{
+  menu.classList.toggle ("menu__list--active");
+});
+
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
